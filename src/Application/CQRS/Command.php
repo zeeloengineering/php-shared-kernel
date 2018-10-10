6407b0c6b4b9430a47c9854750bf2b81ddb2ae8a
Víctor Saavedra Martínez <victor@zeelo.co>
2018-10-10 13:02:29 +0200

    Added like method to Command

--- src/Application/CQRS/Command.php
+++ src/Application/CQRS/Command.php
@@ -9,7 +9,50 @@
 
 namespace StraTDeS\SharedKernel\Application\CQRS;
 
+use StraTDeS\SharedKernel\Domain\EventSourcedEntity;
+
 abstract class Command
 {
+    /**
+     * @param EventSourcedEntity $entity
+     * @return bool
+     * @throws \ReflectionException
+     */
+    public function like(EventSourcedEntity $entity): bool
+    {
+        $commandReflection = new \ReflectionClass($this);
+        $entityReflection = new \ReflectionClass($entity);
 
+        $commandValues = array_map(function(\ReflectionProperty $property) {
+            $property->setAccessible(true);
+            return [
+                'name' => $property->getName(),
+                'value' => $property->getValue($this)
+            ];
+        }, $commandReflection->getProperties());
+
+        $entityValues = array_map(function(\ReflectionProperty $property) use ($entity) {
+            $property->setAccessible(true);
+            return [
+                'name' => $property->getName(),
+                'value' =>
+                    ($property->getName() === 'id') ?
+                        $property->getValue($entity)->toHumandReadable() :
+                        $property->getValue($entity)
+            ];
+        }, $entityReflection->getProperties());
+
+        $changed = false;
+        $i = 0;
+        while ($changed === false && \count($commandValues) > $i) {
+
+            $index = array_search($commandValues[$i]['name'], array_column($entityValues, 'name'), true);
+            if (($index !== false) && ($commandValues[$i]['value'] !== $entityValues[$index]['value'])) {
+                $changed = true;
+            }
+            $i++;
+        }
+
+        return $changed;
+    }
 }
